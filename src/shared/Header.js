2bd3fbf7c4160535fbe7086c31d973724fdca53c
David Barel <>
2021-04-12 12:59:24 -0700

    nav (basic)

--- src/shared/Header.js
+++ src/shared/Header.js
@@ -1,18 +1,43 @@
 import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import logo from "../logo.svg";
 
 export default function Header(): JSX.Element {
   return (
-    <header>
-      <nav className="navbar navbar-light bg-light">
-        <span className="navbar-brand mb-0 h1">
-          <a href="/">
-            <img
-              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrnccpJbEdCSCPpmMILg_s302bDVPF1O0x22O6IrUT7160l1jieeghRgGuEoQx62Krym8&usqp=CAU"
-              alt=""
-            />
-          </a>
-        </span>
-      </nav>
-    </header>
+    <Navbar
+      sticky={"top"}
+      collapseOnSelect
+      expand="lg"
+      bg="dark"
+      variant="dark"
+      className="cs-header navbar navbar-expand-md navbar-dark navbar-sticky navbar-floating"
+    >
+      <Navbar.Brand href="#home">
+        <a className="navbar-brand order-md-1 mr-md-5 mr-0 pr-lg-2" href="/">
+          <img
+            className="navbar-brand-static"
+            src={logo}
+            alt="david barel logo"
+            width="130"
+          />
+          <img
+            className="navbar-brand-floating"
+            src={logo}
+            alt="david barel logo"
+            width="130"
+          />
+        </a>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
